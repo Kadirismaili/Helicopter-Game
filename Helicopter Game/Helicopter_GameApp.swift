1eb0d86bdbd85ffc0c40b7c411c970b936249f6e
Fly <kiwipromm@gmail.com>
2023-03-22 10:13:57 +0100

    Initial Commit

---
+++ Helicopter Game/Helicopter_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Helicopter_GameApp.swift
+//  Helicopter Game
+//
+//  Created by KADIR ISMAILI on 22.3.23.
+//
+
+import SwiftUI
+
+@main
+struct Helicopter_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
